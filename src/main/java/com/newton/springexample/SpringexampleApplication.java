88a3252c53a7f1d0a6e0d3664dca98d2add86726
Neto <newton.neto@capgemini.com>
2021-06-09 15:58:24 -0300

    Testando primeiros requests rest

---
+++ src/main/java/com/newton/springexample/SpringexampleApplication.java
@@ -0,0 +1,13 @@
+package com.newton.springexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringexampleApplication.class, args);
+	}
+
+}
